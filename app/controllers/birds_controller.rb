249f28e5f50e9077e6b26218f715914159c6f4b7
yuki kondo <jinteng.yougui@gmail.com>
2023-05-31 23:04:36 +0900

    controllerでnewを＠birdでdefineしたらエラー発生

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,5 +3,11 @@ class BirdsController < ApplicationController
   end
 
   def new
+    @bird = Bird.new
+  end
+
+  def create
+    Bird.create(content: params[:bird][:content])
+    redirect_to new_bird_path
   end
 end
