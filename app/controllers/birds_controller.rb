e6c878cc8abb5a89fbe4ea4c3cfd8293b6416ca6
yuki kondo <jinteng.yougui@gmail.com>
2023-06-01 17:36:33 +0900

    完成

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,6 @@
 class BirdsController < ApplicationController
+  before_action :set_bird, only: [:show, :edit, :update, :destroy]
+
   def index
     @birds = Bird.all
   end
@@ -9,10 +11,14 @@ class BirdsController < ApplicationController
 
   def create
     @bird = Bird.new(bird_params)
-    if @bird.save
-    redirect_to birds_path, notice: "あなたのつぶやきが作成されました。"
+    if params[:back]
+      render :new 
     else
-      render :new
+      if @bird.save
+        redirect_to birds_path, notice: "あなたのつぶやきが作成されました。"
+      else
+        render :new
+      end
     end
   end
 
@@ -27,10 +33,20 @@ class BirdsController < ApplicationController
   def update
     @bird = Bird.find(params[:id])
     if @bird.update(bird_params)
-      redirect_to birds_path, notice: "編集できました。"
+      redirect_to birds_path, notice:"編集できました。"
     else
       render :edit
     end
+  end
+
+  def destroy
+    @bird.destroy
+    redirect_to birds_path, notice:"削除しました。"
+  end
+
+  def confirm
+    @bird = Bird.new(bird_params)
+    render :new if @bird.invalid?
   end
 
   private
@@ -38,4 +54,9 @@ class BirdsController < ApplicationController
   def bird_params
     params.require(:bird).permit(:content)
   end
+
+  def set_bird
+    @bird = Bird.find(params[:id])
+  end
+
 end
